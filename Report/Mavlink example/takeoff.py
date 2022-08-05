f75df2501613902b00f4615b4492f241112de2b6
TraanQuang1509 <traanquang1509@gmail.com>
2022-08-05 09:01:31 +0700

    first commit

--- Report/Mavlink example/takeoff.py
+++ Report/Mavlink example/takeoff.py
@@ -10,18 +10,18 @@ print("Heartbeat from system (system %u component %u)" %
       (the_connection.target_system, the_connection.target_component))
 
 # Set to arm the motors
-# the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-#                                      mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
 
-# msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-# print(msg)
+msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
 
 # Change mode to Guided mode 
-# the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-#                                      mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, 1, 4, 0, 0, 0, 0, 0)
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, 1, 4, 0, 0, 0, 0, 0)
 
-# msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-# print(msg)
+msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
 
 # Takeoff at Altitude = 10m
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
